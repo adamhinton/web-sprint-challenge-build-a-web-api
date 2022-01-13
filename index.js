a4a17475db80f98c905c50a8e7746410db517e14
adamhinton <adam.d.hinton@gmail.com>
2022-01-13 18:45:38 -0500

    installed express, cors, helmet, dotenv

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+const express = require("express");
+// const cors = require("cors");
