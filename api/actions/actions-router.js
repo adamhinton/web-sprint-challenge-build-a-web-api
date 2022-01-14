6ecf1e16dbb79aaa28f087f093137c50af14b0a5
adamhinton <adam.d.hinton@gmail.com>
2022-01-14 00:43:24 -0500

    passing get api/a

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const Action = require("./actions-model");
 
 router.get("/", (req, res) => {
-  console.log("sucess! 123");
+  Action.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res.status(404).json({ message: "blah blah blah error" });
+    });
 });
 
 module.exports = router;
