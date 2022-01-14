ba281410c38c82ce4013d83a80b884e3555630b4
adamhinton <adam.d.hinton@gmail.com>
2022-01-14 11:11:27 -0500

    wrote skeleton post / action

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -19,4 +19,13 @@ router.get("/:id", validateActionId, async (req, res, next) => {
   res.json(action);
 });
 
+router.post("/", (req, res) => {
+  console.log("posting...");
+});
+
 module.exports = router;
+
+// - [ ] `[POST] /api/actions`
+//   - Returns the newly created action as the body of the response.
+//   - If the request body is missing any of the required fields it responds with a status code 400.
+//   - When adding an action make sure the `project_id` provided belongs to an existing `project`.
